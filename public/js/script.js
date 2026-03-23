2538eb9e8b565ce428755c29c4a4b9eb5d461eec
EyelessCode <cristhianjosue03@outlook.com>
2026-03-23 16:54:44 -0500

    Mejoras en la codificación
    Se integró un método para evitar redundancia en los degrees.

--- public/js/script.js
+++ public/js/script.js
@@ -3,22 +3,27 @@ const hand_minute=document.querySelector(".hand-minute")
 const hand_second=document.querySelector(".hand-second")
 const span=document.querySelector(".dinamic-digital")
 
+const getDegreesCalculation=(value)=>{
+  const degree=((value/60)*360)+90
+  return degree
+}
+
 const getTimeOnClock=()=>{
   const today=new Date()
   const hour=today.getHours()
-  const hourDegrees=((hour/60)*360)+90
-  console.log(`Hora: ${hour}`);
-  hand_hour.style.transform=`rotate(${hourDegrees}deg)`
+  const hourDegrees=getDegreesCalculation(hour)
+  hand_hour.style.transform=`rotate(${
+    hourDegrees}deg)`
   
   const minute=today.getMinutes()
-  const minuteDegrees=((minute/60)*360)+90
-  console.log(`Minutos: ${minute}`);
-  hand_minute.style.transform=`rotate(${minuteDegrees}deg)`
+  const minuteDegrees=getDegreesCalculation(minute)
+  hand_minute.style.transform=`rotate(${
+    minuteDegrees}deg)`
   
   const second=today.getSeconds()
-  const secondDegrees=((second/60)*360)+90
-  console.log(`Segundos: ${second}`);
-  hand_second.style.transform=`rotate(${secondDegrees}deg)`
+  const secondDegrees=getDegreesCalculation(second)
+  hand_second.style.transform=`rotate(${
+    secondDegrees}deg)`
   span.style.display="flex"
   span.innerHTML=
   `
